742090f4b1668eccebc20289dc2153e8fedc7a82
ThanhHai9351 <haihailua123456@gmail.com>
2024-04-08 11:41:44 +0700

    update form

--- src/index.js
+++ src/index.js
@@ -6,24 +6,45 @@ const app = express()
 const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
+app.use(morgan('combined'))// bỏ lại cái nãy đi
 
 
-app.use(morgan('combined'))
+app.engine(
+  "hbs",
+  handlebars({
+    extname: "hbs",
+    layoutsDir: path.join(__dirname, "resources", "views", "layouts"),
+    partialsDir: [ // khác đúng 2 dấu [ ]
+      path.join(__dirname, "resources/views/layouts/partials/"),
+    ],
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources", "views", "layouts"));
 
-app.engine('hbs', handlebars({
-  extname: '.hbs' 
-}));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources', 'views')); 
 
 
 app.get('/', (req, res) => {
-  res.render('home')
+  res.render('home');
 }) 
 
 app.get('/news', (req, res) => {
+  console.log(req.query);
   res.render('news')
-})  
+}) 
+
+app.get('/search', (req, res) => {
+  res.render('search')
+}) 
+
+app.post('/search', (req, res) => {
+  console.log(req.body)
+  res.render('search')
+}) 
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
